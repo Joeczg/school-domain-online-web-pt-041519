172abb314913c1346b4c54c2314961d463c545e0
zhenggao cai <caizhengao1992@gmail.com>
2019-05-07 03:05:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,14 +3,12 @@ class School
   attr_reader :roster
   def initialize(name)
     @name = name
+    @roster = {}
   end
-  def name
-    @name
-  end
-  ROSTER = {}
-  def roster=(roster)
-    @roster = roster
-    ROSTER >> roster
+
+  def roster
+    @roster
+    
   end
   def add_student(student_name,grade)
     
